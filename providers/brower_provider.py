8a7fdd3eb745018dfbed254de70c31a677ea8d70
nexxmobile <nevzatarman@gmail.com>
2019-11-24 20:28:17 +0100

    refactoring wip

--- providers/brower_provider.py
+++ providers/brower_provider.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python3
 from selenium import webdriver
 from pathlib import Path
+import platform
 
 
 def get_brower(brower):
+    user_path = get_user_data_dir(brower)
     if(brower == 'chrome'):
-        chrome_user_path = str(Path.joinpath(
-            Path.home(), 'AppData\\Local\\Google\\Chrome\\User Data'))
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument("user-data-dir=%s" % chrome_user_path)
-        return webdriver.Chrome('D:/chromedriver.exe',  # todo change this
-                                options=chrome_options)
+        if(len(user_path) > 0):
+            chrome_options.add_argument("user-data-dir=%s" % user_path)
+        return webdriver.Chrome(options=chrome_options)
     elif brower == 'firefox':
-        firefox_user_path = str(Path.joinpath(
-            Path.home(), 'AppData\\Roaming\\Mozilla\\Firefox\\Profiles'))
         firefox_options = webdriver.FirefoxOptions()
-        firefox_options.add_argument("user-data-dir=%s" % firefox_user_path)
-        # todo change this
-        return webdriver.Firefox(executable_path='D:/geckodriver.exe', options=firefox_options)
+        if(len(user_path) > 0):
+            firefox_options.add_argument("user-data-dir=%s" % user_path)
+        return webdriver.Firefox(options=firefox_options)
     raise Exception(
-        'Please type in a browser name, such as chrome, firefox, opera...')
+        'Please type in a browser name, such as chrome or firefox.')
+
+
+def get_user_data_dir(browser):
+    pl = platform.system()
+    if(pl == 'Windows'):
+        if(browser == 'chrome'):
+            return str(Path.joinpath(
+                Path.home(), 'AppData\\Local\\Google\\Chrome\\User Data'))
+        elif(browser == 'firefox'):
+            return str(Path.joinpath(
+                Path.home(), 'AppData\\Roaming\\Mozilla\\Firefox\\Profiles'))
+    elif(pl == 'Darwin'):
+        pass
+    elif(pl == 'Linux'):
+        pass  # pass for now
